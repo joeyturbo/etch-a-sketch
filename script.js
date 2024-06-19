01c44457e7090f2485328cb5d2dd8443a1e0f435
joeyturbo <joeyturbo@gmail.com>
2024-06-19 16:22:07 -0400

    cleaned up code to add createNewGrid function which calculates grid size, iteratively creates grid squares and applies class/ids, sets flex-basis, adds to container and adds event listener

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 
 defaultGridSize = 256;
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const gridSquare = document.querySelectorAll('.square');
+const button = document.querySelector('#gridBtn');
 
-for (i = 1; i <= defaultGridSize; i++) {
-  const square = document.createElement('div');
-  square.setAttribute('class', 'square');
-  square.setAttribute('id',`square-${i}`);
-  container.appendChild(square);
+function createNewGrid(gridSize) {
+  const gridTotal = gridSize**2;
+  for (i = 1; i <= gridTotal; i++) {
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    square.setAttribute('id',`square-${i}`);
+    square.style.flexBasis = `${100 / gridSize}%`;
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+      square.classList.add('hovered');
+    })
+  } 
 }
 
-const gridSquare = document.querySelectorAll('.square');
-
-gridSquare.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.classList.add('hovered');
-  })
+button.addEventListener('click', () => {
+  container.innerHTML = '';
+  let newGridSize = prompt('Please enter the grid size you want (must be under 100):');
+  if (newGridSize > 100) {
+    newGridSize = alert(`${newGridSize} is over 100. Try again!`)
+    createNewGrid(16);
+  } else {
+    createNewGrid(newGridSize);
+  }
 })
+
+window.addEventListener('load', createNewGrid(16));
