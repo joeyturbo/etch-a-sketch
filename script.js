acca9024045d8cd0e20ba8eebf1493b91154c07e
joeyturbo <joeyturbo@gmail.com>
2024-06-20 14:45:15 -0400

    added eventlistener inside of draw function to lower opacity on mouseover

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 
 const container = document.querySelector('.container');
-const gridSquare = document.querySelectorAll('.square');
+const gridSquares = document.querySelectorAll('.square');
 const button = document.querySelector('#gridBtn');
 
+
+// random rgb color generator`
 function randomColorGenerator(min=0, max=255) {
   let rgbArray = [];
   for (i = 1; i <= 3; i++) {
@@ -14,6 +16,20 @@ function randomColorGenerator(min=0, max=255) {
   return `rgb(${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]})`;
 }
 
+// function darkenSquare(squareId) {
+//   const square = document.querySelector(`#${squareId}`);
+//   square.style.opacity = square.style.opacity - .1;
+//   console.log(square.style.opacity)
+// }
+
+function draw(squareId) {
+  const square = document.querySelector(`#${squareId}`)
+  square.style.backgroundColor = randomColorGenerator();
+  square.addEventListener('mouseover', () => {
+    square.style.opacity = String(Number(square.style.opacity) - .02);
+  })
+}
+
 function createNewGrid(gridSize) {
   // calculate total grid size
   const gridTotal = gridSize**2;
@@ -22,11 +38,11 @@ function createNewGrid(gridSize) {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
     square.setAttribute('id',`square-${i}`);
+    square.style.opacity = "1";
     square.style.flexBasis = `${100 / gridSize}%`;
     container.appendChild(square);
     square.addEventListener('mouseover', () => {
-      // square.classList.add('hovered');
-      square.style.backgroundColor = randomColorGenerator();
+      draw(square.id);
     })
   } 
 }
@@ -46,6 +62,9 @@ button.addEventListener('click', () => {
     createNewGrid(newGridSize);
   }
 })
+
+
+
 
 // loads default 16x16 grid
 window.addEventListener('load', createNewGrid(16));
